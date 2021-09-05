f178251fbb128b3c599a3c6bc07bf9aa8e528c4c
yash <33136040+naturephile@users.noreply.github.com>
2021-09-05 14:09:08 +0530

    Add files via upload

--- list_all.bat
+++ list_all.bat
@@ -1,13 +1,18 @@
+:: Scr1pt by Y@5#, github.com/naturephile
+
 @echo off
 
 echo "enter your desirable PATH to get list of all files and sub folders.."
+echo "Press "enter" for current path."
+
 set /p loc=:
 dir %loc% /b /s > list-all.txt
 echo 
 echo "whole list is stored in 'list-all.txt' file, in current dir."
-timeout 6
+timeout 2
 
 set "loc="
 
 exit 0
+
 :: Scr1pt by Y@5#, github.com/naturephile
